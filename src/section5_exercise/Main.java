1cb91088fe5686954e7dad794a9d457f8b45ab7d
SunghoonKim <kootae88@gmail.com>
2023-04-07 22:58:29 +0900

    exercise - one

---
+++ src/section5_exercise/Main.java
@@ -0,0 +1,8 @@
+package section5_exercise;
+
+public class Main {
+
+  public static void main(String[] args) {
+    PositiveNegativeZero.checkNumber(-1);
+  }
+}
---
+++ src/section5_exercise/PositiveNegativeZero.java
@@ -0,0 +1,17 @@
+package section5_exercise;
+
+public class PositiveNegativeZero {
+  private PositiveNegativeZero(){
+    throw new IllegalStateException("Utility Class");
+  }
+
+  public static void checkNumber(int number) {
+    if (number > 0) {
+      System.out.println("positive");
+    } else if (number < 0) {
+      System.out.println("negative");
+    } else {
+      System.out.println("zero");
+    }
+  }
+}
