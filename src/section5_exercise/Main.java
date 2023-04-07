e61d37991c6b8814397c5c6acfe3f996874e2e83
SunghoonKim <kootae88@gmail.com>
2023-04-08 04:34:52 +0900

    exercises-main

--- src/section5_exercise/Main.java
+++ src/section5_exercise/Main.java
@@ -6,8 +6,8 @@ public class Main {
     PositiveNegativeZero.checkNumber(-1);
     SpeedConverter.printConversion(75.114);
     MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
-    System.out.println(LeapYearCalculator.isLeapYear(2000));
     System.out.println(BarkingDog.shouldWakeUp(true, 23));
+    System.out.println(LeapYearCalculator.isLeapYear(2000));
     System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.1416345, 3.14167456));
   }
 }
