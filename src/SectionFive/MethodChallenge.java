6be6b67d1800d43b900f3f5fd4280e0b4743e8a9
SunghoonKim <kootae88@gmail.com>
2023-04-07 21:38:26 +0900

    section5

--- src/SectionFive/MethodChallenge.java
+++ src/SectionFive/MethodChallenge.java
@@ -16,14 +16,16 @@ public class MethodChallenge {
   }
 
   public static int calculateHighScorePosition(int score) {
+    int position = 4;
+
     if (score >= 1000) {
-      return 1;
-    } else if ((score < 1000) && (score >= 500)) {
-      return 2;
-    } else if ((score < 500) && (score >= 100)) {
-      return 3;
-    } else {
-      return 4;
+      position = 1;
+    } else if (score >= 500) {
+      position = 2;
+    } else if (score >= 100) {
+      position = 3;
     }
+
+    return position;
   }
 }
