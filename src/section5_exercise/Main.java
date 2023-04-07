1bf3b6a1e96fc00f34537f22b7eaff6e7f21b205
SunghoonKim <kootae88@gmail.com>
2023-04-08 00:21:06 +0900

    (no branch): Auto stash before revert of "exercise - three"

--- src/section5_exercise/Main.java
+++ src/section5_exercise/Main.java
@@ -5,5 +5,6 @@ public class Main {
   public static void main(String[] args) {
     PositiveNegativeZero.checkNumber(-1);
     SpeedConverter.printConversion(75.114);
+    MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
   }
 }
