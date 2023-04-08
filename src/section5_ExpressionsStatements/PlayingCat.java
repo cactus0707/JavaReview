a6b3696ed43d5a269d18303ecb6db8aeabafb0dd
SunghoonKim <kootae88@gmail.com>
2023-04-09 03:39:20 +0900

    PlayingCat - done

---
+++ src/section5_ExpressionsStatements/PlayingCat.java
@@ -0,0 +1,18 @@
+package section5_ExpressionsStatements;
+
+public class PlayingCat {
+
+  /*
+   * playing between 25 and 35 temperature(inclusive)
+   * playing between 25 and 45 temperature(inclusive) - summer
+   * */
+  public static boolean isCatPlaying(boolean summer,
+                             int temperature) {
+    if (summer) {
+      return (temperature >= 25) && (temperature <= 45);
+    } else {
+      return (temperature >= 25) && (temperature <= 35);
+    }
+  }
+
+}
