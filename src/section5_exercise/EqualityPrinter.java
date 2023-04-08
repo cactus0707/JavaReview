23f42f8c313d7676b6a4e402847f275d3f46c772
SunghoonKim <kootae88@gmail.com>
2023-04-09 01:31:45 +0900

    EqualityPrinter - done

---
+++ src/section5_exercise/EqualityPrinter.java
@@ -0,0 +1,24 @@
+package section5_exercise;
+
+public class EqualityPrinter {
+
+  public static void printEqual(int numberA,
+                                int numberB,
+                                int numberC) {
+
+    if (numberA < 0 || numberB < 0 || numberC < 0) {
+      System.out.println("Invalid Value");
+      return;
+    }
+
+    if (numberA - numberB != 0 && numberB - numberC != 0 && numberC - numberA != 0) {
+      System.out.println("All numbers are different");
+    } else if (numberA - numberB != 0 || numberB - numberC != 0 || numberC - numberA != 0) {
+      System.out.println("Neither all are equal or different");
+    } else if (numberA - numberB == 0 && numberB - numberC == 0 && numberC - numberA == 0) {
+      System.out.println("All numbers are equal");
+    }
+  }
+
+
+}
