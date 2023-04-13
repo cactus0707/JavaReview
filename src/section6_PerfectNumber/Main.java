3db69111b58ebc8f4299423d6954b3565af90fae
SunghoonKim <kootae88@gmail.com>
2023-04-14 04:17:55 +0900

    isPerfectNumber

--- src/section6_PerfectNumber/Main.java
+++ src/section6_PerfectNumber/Main.java
@@ -1,5 +1,17 @@
 package section6_PerfectNumber;
 
 public class Main {
+  public static boolean isPerfectNumber(int number) {
+    if (number < 1) {
+      return false;
+    }
+    int sumOfFactor = 0;
+    for (int factor = 1; factor <= number; factor++) {
+      if (number % factor == 0 && factor !=number) {
+        sumOfFactor += factor;
+      }
+    }
+    return sumOfFactor == number;
+  }
 
 }
