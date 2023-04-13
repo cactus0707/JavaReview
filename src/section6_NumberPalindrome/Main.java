fd19f4978e0cd6dfb1971e9421c5bb153e16ed7a
SunghoonKim <kootae88@gmail.com>
2023-04-14 01:28:28 +0900

    isPalindrome

--- src/section6_NumberPalindrome/Main.java
+++ src/section6_NumberPalindrome/Main.java
@@ -1,5 +1,21 @@
 package section6_NumberPalindrome;
 
+import java.util.Stack;
+
 public class Main {
 
+  public static boolean isPalindrome(int number) {
+    int reverseNumber = 0;
+    Stack<Integer> palidromeNumber = new Stack<Integer>();
+
+    while (number!=0) {
+      palidromeNumber.push((Integer) number % 10);
+      number /= 10;
+    }
+    while (palidromeNumber.capacity()!=0) {
+      reverseNumber += (int) palidromeNumber.pop() + palidromeNumber.capacity();
+    }
+    reverseNumber += palidromeNumber.pop();
+    return reverseNumber - number == 0;
+  }
 }
