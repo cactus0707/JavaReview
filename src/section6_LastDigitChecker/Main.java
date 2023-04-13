88623f36ce8ecbf9368b942118ecf284ac418e48
SunghoonKim <kootae88@gmail.com>
2023-04-14 03:06:36 +0900

    hasSameRightMost

--- src/section6_LastDigitChecker/Main.java
+++ src/section6_LastDigitChecker/Main.java
@@ -2,4 +2,22 @@ package section6_LastDigitChecker;
 
 public class Main {
 
+  public static boolean hasSameLastDigit(int numberA,
+                                         int numberB,
+                                         int numberC) {
+    if (!(isValid(numberA) && isValid(numberB) && isValid(numberC))) {
+      return false;
+    }
+
+    int rightmostNumberA = numberA % 10;
+    int rightmostNumberB = numberB % 10;
+    int rightmostNumberC = numberC % 10;
+
+    return rightmostNumberA == rightmostNumberB || rightmostNumberA == rightmostNumberC
+        || rightmostNumberB == rightmostNumberC;
+  }
+
+  public static boolean isValid(int number) {
+    return number >= 10 && number <= 1000;
+  }
 }
