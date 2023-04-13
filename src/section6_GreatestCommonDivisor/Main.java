5f4241430fcbca0b474680fb3de9ae03a7b26ee3
SunghoonKim <kootae88@gmail.com>
2023-04-14 03:57:28 +0900

    getGreatestCommonDivisor

--- src/section6_GreatestCommonDivisor/Main.java
+++ src/section6_GreatestCommonDivisor/Main.java
@@ -2,4 +2,20 @@ package section6_GreatestCommonDivisor;
 
 public class Main {
 
+  public static int getGreatestCommonDivisor(int numberA,
+                                             int numberB) {
+    if (!((numberA>=10) && (numberB>=10))) {
+      return -1;
+    }
+    int temp = 0;
+    while (numberB - numberA !=0) {
+      if (numberA > numberB) {
+        temp = numberA;
+        numberA = numberB;
+        numberB = temp;
+      }
+      numberB = numberB - numberA;
+    }
+    return numberB;
+  }
 }
