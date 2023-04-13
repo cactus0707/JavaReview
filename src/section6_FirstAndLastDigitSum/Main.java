519e7590296c901b27aeaf54984885ffb2aeb7c9
SunghoonKim <kootae88@gmail.com>
2023-04-14 01:56:27 +0900

    FirstAndLastDigitSum

--- src/section6_FirstAndLastDigitSum/Main.java
+++ src/section6_FirstAndLastDigitSum/Main.java
@@ -2,4 +2,27 @@ package section6_FirstAndLastDigitSum;
 
 public class Main {
 
+  public static void main(String[] args) {
+  }
+
+  public static int sumFirstAndLastDigit(int number) {
+    if (number < 0) {
+      return -1;
+    }
+    int sum = 0;
+    sum += number % 10;
+
+    while (true) {
+      if (number / 10 == 0) {
+        sum += number;
+        break;
+      }
+      if (number == 0) {
+        sum += number;
+        break;
+      }
+      number = number / 10;
+    }
+    return sum;
+  }
 }
