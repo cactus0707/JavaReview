24f836010dc995a9c7a70f6611d56acfc20c505a
SunghoonKim <kootae88@gmail.com>
2023-04-14 02:44:14 +0900

    hasSharedDigit

--- src/section6_SharedDigit/Main.java
+++ src/section6_SharedDigit/Main.java
@@ -1,5 +1,28 @@
 package section6_SharedDigit;
 
+import java.util.HashSet;
+
 public class Main {
 
+  public static boolean hasSharedDigit(int numberA,
+                                       int numberB) {
+    if (!(((numberA >= 10) && (numberA <= 99)) && ((numberB >= 10) && (numberB <= 99)))) {
+      return false;
+    }
+    HashSet<Integer> hashSet = new HashSet<>();
+    while (numberA != 0) {
+      hashSet.add(numberA % 10);
+      numberA = numberA / 10;
+    }
+
+    boolean contains;
+    while (numberB != 0) {
+      contains = hashSet.contains(numberB % 10);
+      numberB = numberB / 10;
+      if (contains) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
