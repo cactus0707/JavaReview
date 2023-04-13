e84d18ba076b6495d9b75ed1a0ddb1171832de4e
SunghoonKim <kootae88@gmail.com>
2023-04-14 02:07:45 +0900

    EvenDigitSum

--- src/section6_EvenDigitSum/Main.java
+++ src/section6_EvenDigitSum/Main.java
@@ -2,4 +2,25 @@ package section6_EvenDigitSum;
 
 public class Main {
 
+  public static int getEvenDigitSum(int number) {
+
+    if (number < 0) {
+      return -1;
+    }
+
+    int sumOfEvenDigit = 0;
+    int remainder = 0;
+    while (true) {
+      remainder = number % 10;
+      if (remainder % 2 == 0) {
+        sumOfEvenDigit += remainder;
+      }
+      if (number == 0) {
+        break;
+      }
+      number = number / 10;
+    }
+    return sumOfEvenDigit;
+  }
+
 }
