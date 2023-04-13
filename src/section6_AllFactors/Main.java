cbaef9138ead6e520de1d45d2ebc813180c74efb
SunghoonKim <kootae88@gmail.com>
2023-04-14 03:40:48 +0900

    All Factors

--- src/section6_AllFactors/Main.java
+++ src/section6_AllFactors/Main.java
@@ -2,4 +2,15 @@ package section6_AllFactors;
 
 public class Main {
 
+  public static void printFactors(int number) {
+    if (number < 1) {
+      System.out.println("Invalid Value");
+      return;
+    }
+    for (int factor = 1; factor <= number; factor++) {
+      if (number % factor == 0) {
+        System.out.println(factor);
+      }
+    }
+  }
 }
