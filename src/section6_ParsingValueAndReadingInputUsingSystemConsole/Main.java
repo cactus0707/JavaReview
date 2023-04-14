81baacec824a8aab1820507c96f7a687173a873f
SunghoonKim <kootae88@gmail.com>
2023-04-15 06:12:02 +0900

    Readme 수정 및 Exception Handling

--- src/section6_ParsingValueAndReadingInputUsingSystemConsole/Main.java
+++ src/section6_ParsingValueAndReadingInputUsingSystemConsole/Main.java
@@ -31,9 +31,30 @@ public class Main {
     String name = scanner.nextLine();
     System.out.println("Hi " + name + ", Thank for taking the course!");
     System.out.println("What year were you born?");
-    int dateOfBirth = Integer.parseInt(scanner.nextLine());
-    int age = currentYear - dateOfBirth;
+
+    boolean validDOB = false;
+    int age = 0;
+    do {
+      System.out.println(
+          "Enter a year of birth >=" + (currentYear - 125) + " and <= " + (currentYear));
+      try {
+        age = checkData(currentYear, scanner.nextLine());
+        validDOB = age >= 0;
+      } catch (NumberFormatException badUserData) {
+        System.out.println("Characters not allowed!! Try again");
+      }
+    } while (!validDOB);
+
     return "So you are " + age + " years old";
   }
 
+  public static int checkData(int currentYear,
+                              String dateOfBirth) {
+    int dob = Integer.parseInt(dateOfBirth);
+    int minimumYear = currentYear - 125;
+    if ((dob < minimumYear) || (dob > currentYear)) {
+      return -1;
+    }
+    return (currentYear - dob);
+  }
 }
